2ebdf1a18bf27bb329bdb7cf8f6322d01859deae
Erick PC <erksters@gmail.com>
2021-03-06 23:43:03 -0600

    Added level for with good horizontal mechanics

---
+++ Game3/Screens/GamePlayScreen4.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Game3;
+using GameArchitectureExample.StateManagement;
+using tainicom.Aether.Physics2D.Collision.Shapes;
+using tainicom.Aether.Physics2D.Common;
+using tainicom.Aether.Physics2D.Dynamics;
+using tainicom.Aether.Physics2D.Dynamics.Joints;
+using GameArchitectureExample.Screens;
+
+namespace Game3.Screens
+{
+    public class GamePlayScreen4 : GameScreen
+    {
+        private ContentManager _content;
+        private SpriteFont _gameFont;
+
+        #region Transition animations
+        private float _pauseAlpha;
+        private readonly InputAction _pauseAction;
+        #endregion
+
+        #region Game Contents
+        private Protagonist4 protagonist;
+        private World world;
+        private Vector2 initialPosition = new Vector2(300,300);
+        private Vector2 gravityForce = new Vector2(0, 100);
+        #endregion
+
+        /// <summary>
+        /// My public constructor
+        /// </summary>
+        public GamePlayScreen4()
+        {
+            if (!Constants.inDevelopment)
+            {
+                TransitionOnTime = TimeSpan.FromSeconds(1.5);
+                TransitionOffTime = TimeSpan.FromSeconds(0.5);
+            }
+            else
+            {
+                TransitionOnTime = TimeSpan.FromSeconds(0);
+                TransitionOffTime = TimeSpan.FromSeconds(0);
+            }
+
+            _pauseAction = new InputAction(
+                new[] { Buttons.Start, Buttons.Back },
+                new[] { Keys.Back, Keys.Escape, Keys.P }, true);
+
+            world = new World();
+            world.Gravity = gravityForce;
+            //world.Gravity = new Vector2(0, 0);
+            GenerateBoundaries();
+            var RigidBody = world.CreateRectangle(Protagonist4.idleWidth, Protagonist4.idleHeight,
+                10, initialPosition, 0, BodyType.Dynamic);
+            RigidBody.SetRestitution(0);
+            protagonist = new Protagonist4(RigidBody, world);
+        }
+
+        /// <summary>
+        /// Load in graphics for this Game Plays Screen
+        /// </summary>
+        public override void Activate()
+        {
+            if (_content == null) { _content = new ContentManager(ScreenManager.Game.Services, "Content"); }
+            
+            _gameFont = _content.Load<SpriteFont>("gamefont");
+            protagonist.LoadContent(_content);
+        }
+
+        private void GenerateBoundaries()
+        {
+            var top = 0;
+            var bottom = Constants.GAME_HEIGHT - 50;
+            var left = 0;
+            var right = Constants.GAME_WIDTH;
+            var edges = new Body[]
+            {
+                world.CreateEdge(new Vector2(left, top), new Vector2(right, top)),
+                world.CreateEdge(new Vector2(right, top), new Vector2(right, bottom)),
+                world.CreateEdge(new Vector2(left, bottom), new Vector2(right, bottom)),
+                world.CreateEdge(new Vector2(left, top), new Vector2(left, bottom )),
+            };
+            foreach (var edge in edges)
+            {
+                edge.BodyType = BodyType.Static;
+                edge.SetRestitution(0);
+            };
+        }
+
+        // This method checks the GameScreen.IsActive property, so the game will
+        // stop updating when the pause menu is active, or if you tab away to a different application.
+        public override void Update(GameTime gameTime, bool otherScreenHasFocus, bool coveredByOtherScreen)
+        {
+            base.Update(gameTime, otherScreenHasFocus, false);
+
+            // Gradually fade in or out depending on whether we are covered by the pause screen.
+            HandlePauseTransition(coveredByOtherScreen);
+
+            //If this screen is active
+            if (IsActive)
+            {
+                //TODO: Add your games update methods here 
+                world.Step((float)gameTime.ElapsedGameTime.TotalSeconds);
+
+            }
+        }
+
+        public override void HandleInput(GameTime gameTime, InputState input)
+        {
+
+            PlayerIndex player;
+
+            //Do we need to pause?
+            if (_pauseAction.Occurred(input, ControllingPlayer, out player))
+            {
+                ScreenManager.AddScreen(new PauseMenuScreen(), ControllingPlayer);
+            }
+
+            protagonist.Update(gameTime, input);
+
+
+        }
+
+
+        public override void Draw(GameTime gameTime)
+        {
+            // This game has a blue background. Why? Because!
+            ScreenManager.GraphicsDevice.Clear(ClearOptions.Target, Color.CornflowerBlue, 0, 0);
+
+
+            // Our player and enemy are both actually just text strings.
+            var spriteBatch = ScreenManager.SpriteBatch;
+
+            spriteBatch.Begin();
+            protagonist.Draw(gameTime, spriteBatch);
+            spriteBatch.End();
+
+            // If the game is transitioning on or off, fade it out to black.
+            HandleScreenTransition();
+        }
+
+        /// <summary>
+        /// Helper method to animate a transition from another screen (MenuScreen)
+        /// Used in Draw()
+        /// </summary>
+        private void HandleScreenTransition()
+        {
+            if (TransitionPosition > 0 || _pauseAlpha > 0)
+            {
+                float alpha = MathHelper.Lerp(1f - TransitionAlpha, 1f, _pauseAlpha / 2);
+
+                ScreenManager.FadeBackBufferToBlack(alpha);
+            }
+        }
+
+        /// <summary>
+        /// Helper method to handle animations for pausing the gamescreen
+        /// Used in HandleInput()
+        /// </summary>
+        /// <param name="coveredByOtherScreen">Tell this method if we are fading in or out</param>
+        private void HandlePauseTransition(bool coveredByOtherScreen)
+        {
+            if (coveredByOtherScreen)
+                _pauseAlpha = Math.Min(_pauseAlpha + 1f / 32, 1);
+            else
+                _pauseAlpha = Math.Max(_pauseAlpha - 1f / 32, 0);
+        }
+
+        /// <summary>
+        /// Windows phone requirement
+        /// </summary>
+        public override void Deactivate()
+        {
+            base.Deactivate();
+        }
+
+        /// <summary>
+        /// Windows phone requirement
+        /// </summary>
+        public override void Unload()
+        {
+            _content.Unload();
+        }
+    }
+}
--- Game3/Screens/LevelSelectScreen.cs
+++ Game3/Screens/LevelSelectScreen.cs
@@ -15,16 +15,19 @@ namespace Game3.Screens
             var Level1Entry = new MenuEntry("1");
             var Level2Entry = new MenuEntry("2");
             var Level3Entry = new MenuEntry("3");
+            var Level4Entry = new MenuEntry("4");
             var exitMenuEntry = new MenuEntry("Back");
 
             Level1Entry.Selected += Level1EntrySelected;
             Level2Entry.Selected += Level2EntrySelected;
             Level3Entry.Selected += Level3EntrySelected;
+            Level4Entry.Selected += Level4EntrySelected;
             exitMenuEntry.Selected += OnCancel;
 
             MenuEntries.Add(Level1Entry);
             MenuEntries.Add(Level2Entry);
             MenuEntries.Add(Level3Entry);
+            MenuEntries.Add(Level4Entry);
             MenuEntries.Add(exitMenuEntry);
 
         }
@@ -57,6 +60,16 @@ namespace Game3.Screens
         private void Level3EntrySelected(object Sender, PlayerIndexEventArgs e)
         {
             LoadingScreen.Load(ScreenManager, true, e.PlayerIndex, new GamePlayScreen3());
+        }
+
+        /// <summary>
+        /// Send the user to a GamePlayScreen #
+        /// </summary>
+        /// <param name="Sender"></param>
+        /// <param name="e"></param>
+        private void Level4EntrySelected(object Sender, PlayerIndexEventArgs e)
+        {
+            LoadingScreen.Load(ScreenManager, true, e.PlayerIndex, new GamePlayScreen4());
         }
     }
 }
